8523bb0d58ff0715565a17014de4b6f2bab44af4
Mohammad Hossein <m.h.tabatabai23@gmail.com>
2026-05-14 17:05:47 +0330

    add blog app
    added blog app with 2 html files
    +blog-home
    +blog-single

--- blog/urls.py
+++ blog/urls.py
@@ -14,17 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path
+from blog.views import *
 
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
+app_name = "blog"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("website/", include("website.urls")),
-    path("blog/", include("blog.urls")),
+    path("", blog, name="blog"),
+    path("single/", single_blog, name="single"),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
