92e6748da62d5cf3bb456f6f1a83df6fcc35685b
Mohammad Hossein <m.h.tabatabai23@gmail.com>
2026-05-15 18:52:52 +0330

    add models post and contact
    create two models
    post model in blog app
    contact model in website app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    # author
+    # image
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # category
+    # tag
+    counted_view = models.IntegerField(default= 0)
+    status = models.BooleanField(null=True)
+    published_date = models.DateTimeField(default = timezone.now)
+    created_date = models.DateTimeField(auto_now_add= True)
+    updated_date = models.DateTimeField(auto_now= True)
