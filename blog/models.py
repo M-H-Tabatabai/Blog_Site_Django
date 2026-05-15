20d51085a28c0401f2a235c7f6037ceaf5fdf299
Mohammad Hossein <m.h.tabatabai23@gmail.com>
2026-05-15 21:09:03 +0330

    add meta class

--- blog/models.py
+++ blog/models.py
@@ -15,5 +15,9 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add= True)
     updated_date = models.DateTimeField(auto_now= True)
 
+    class Meta:
+        ordering = ['-created_date']
+
+
     def __str__(self):
         return f"title : {self.title} - id : {self.id}"
