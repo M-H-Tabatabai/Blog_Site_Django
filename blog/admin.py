898ae576ef6d1da02981a11aac58405170751c0a
Mohammad Hossein <m.h.tabatabai23@gmail.com>
2026-05-17 17:49:18 +0330

    add author field
    added author filed in post model and formatting style code

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 from blog.models import Post
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status','counted_view', 'created_date', 'published_date', 'updated_date')
-    list_filter = ('status', 'created_date')
-    search_fields = ('title', 'content')
-    list_editable = ('status',)
-    ordering = ('-created_date',)
-    list_per_page = 20  # نمایش ۲۰ رکورد در هر صفحه
-    date_hierarchy = 'created_date'
 
-    
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "status",
+        "counted_view",
+        "created_date",
+        "published_date",
+        "updated_date",
+    )
+    list_filter = ("status", "created_date", "author")
+    search_fields = ("title", "content", "author")
+    list_editable = ("status",)
+    ordering = ("-created_date",)
+    list_per_page = 20  # نمایش ۲۰ رکورد در هر صفحه
+    date_hierarchy = "created_date"
+
+
 admin.site.register(Post, PostAdmin)
