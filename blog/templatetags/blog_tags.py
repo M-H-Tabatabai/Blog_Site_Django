8989172083c431131b8b34b71029cc6faab81574
Mohammad Hossein <m.h.tabatabai23@gmail.com>
2026-05-18 20:35:15 +0330

    add custom tags
    added custom tags in blog app.
    latest - latest2 - slicer

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from blog.models import Post
 
 register = template.Library()
 
@@ -6,4 +7,23 @@ register = template.Library()
 def counter (context):
     request = context.get("request")
     return f"method : {request.method} \\ user : {request.user}"
+
+@register.simple_tag(name= "latest")
+def latest_post():
+    posts = Post.objects.all()[:2]
+    post_list = []
+    for post in posts:
+        post_list.append(post.title)
+    return f"post list = {post_list}"
+
+@register.simple_tag(name="latest2")
+def latest_post2(posts):
+    post_list = []
+    for post in posts:
+        post_list.append(post.title)
+    return post_list[:2]
+
+@register.filter(name = "slicer")
+def end_slicer(text):
+    return text[:5] + "..."
 
