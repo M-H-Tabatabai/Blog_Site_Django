79e6090e26074b0e2ab427f6e6d66f96c22c13ad
Mohammad Hossein <m.h.tabatabai23@gmail.com>
2026-05-23 19:59:26 +0330

    install 3 apps in mysite
    added 3 apps to poject
    1- site
    2-sitemap
    3-humanize

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -31,3 +32,7 @@ class Post(models.Model):
 
     def __str__(self):
         return f"title(obj) : {self.title}"
+    
+    def get_absolute_url(self):
+        return reverse("blog:single",kwargs={'pid':self.id} )
+    
