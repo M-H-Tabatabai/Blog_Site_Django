d513056c08dba6426494f2392b6e094528e85a6a
Mohammad Hossein <m.h.tabatabai23@gmail.com>
2026-05-27 17:03:48 +0330

    add django debug toolbar

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    "debug_toolbar",
     'robots',
     "website.apps.WebsiteConfig",
     "blog.apps.BlogConfig",
@@ -56,7 +57,13 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
+INTERNAL_IPS = [
+        "127.0.0.1",
+    ]
+
 
 ROOT_URLCONF = "mysite.urls"
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from website.sitemaps import StaticViewSitemap
 from blog.sitemaps import BlogSitemap
+import debug_toolbar
 sitemaps = {
     'static': StaticViewSitemap,
     'blog': BlogSitemap,
@@ -34,6 +35,7 @@ urlpatterns = [
     path('robots.txt', include('robots.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
