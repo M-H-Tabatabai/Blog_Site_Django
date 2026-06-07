f8a1f5295bc24d28cb1e6159aec3eae5c945e07b
Mohammad Hossein <m.h.tabatabai23@gmail.com>
2026-06-07 15:53:55 +0330

    create signup page

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ app_name = "accounts"
 urlpatterns = [
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
-    # path("signup", signup_view, name="signup"),
+    path("signup", signup_view, name="signup")
 ]
